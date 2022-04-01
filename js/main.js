a0b3b39ce3fff544341770083026410dc85ee07d
asomethingk <somethingliketa@gmail.com>
2022-04-01 20:44:24 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log("Hello world");
+let message = "Hello";
+const array = [1, 1, 3, 4, 5, 6, 7, 8, 9, 10]
+const result = array.map(function (item) {
+  return item * 2;
+});
+
+console.log(result);
