3d6eca30f2914a84b2d29dc4c09b6b0e64a60601
asomethingk <somethingliketa@gmail.com>
2022-04-05 14:40:25 +0900

    sixth

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-console.log("Hello world");
-let message = "Hello";
-const array = [1, 1, 3, 4, 5, 6, 7, 8, 9, 10]
-const result = array.map(function (item) {
-  return item * 2;
+const target = document.getElementById("header__nav");
+target.addEventListener('click', () => {
+  const target = document.getElementById("header__nav");
+  target.classList.toggle('open');
+  const nav = document.getElementById("header__nav");
+  nav.classList.toggle('in');
 });
-
-console.log(result);
