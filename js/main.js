be48062957b81a0db20052c345db600981da6f40
asomethingk <somethingliketa@gmail.com>
2022-04-05 17:38:13 +0900

    seventh

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-const target = document.getElementById("header__nav");
+const target = document.getElementById("menu");
 target.addEventListener('click', () => {
-  const target = document.getElementById("header__nav");
+  const target = document.getElementById("menu");
   target.classList.toggle('open');
-  const nav = document.getElementById("header__nav");
+  const nav = document.getElementById("drawer-nav");
   nav.classList.toggle('in');
 });
